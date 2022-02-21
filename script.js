d5686d99e32b4058937676153edb3b22c59c4a89
Jas <hacyonukumori@gmail.com>
2022-02-21 21:19:41 +0800

    calculator

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const clear = document.querySelector('.clear')
 const del = document.querySelector('.delete')
 const overoutput = document.querySelector('.overoutput')
 const underoutput = document.querySelector('.underoutput')
+const tempoutput = document.querySelector('.tempoutput')
 
 let output1 = ''
 let output2 = ''
@@ -41,21 +42,108 @@ operation.forEach( (operation) => {
 
 function clearVar(name = '') {
   output1 += output2 + ' ' + name + ' '
-  underoutput.innerText = output1
-  overoutput.innerText = ''
+  overoutput.innerText = output1
+  underoutput.innerText = ''
   output2 = ''
+  tempoutput.innerText = result
 }
 
 function mathOperation() {
-  if (lastOperation === "x") {
+  if (operationLast === "x") {
     result = parseFloat(result) * parseFloat(output2);
-  } else if (lastOperation === "+") {
+  } else if (operationLast === "+") {
     result = parseFloat(result) + parseFloat(output2);
-  } else if (lastOperation === "-") {
+  } else if (operationLast === "-") {
     result = parseFloat(result) - parseFloat(output2);
-  } else if (lastOperation === "/") {
+  } else if (operationLast === "/") {
     result = parseFloat(result) / parseFloat(output2);
-  } else if (lastOperation === "%") {
+  } else if (operationLast === "%") {
     result = parseFloat(result) % parseFloat(output2);
   }
+}
+
+resultLast.addEventListener('click', (e) => {
+  if ( !output1 || !output2 ) return
+  oneDot = false
+  mathOperation()
+  clearVar()
+  underoutput.innerText = result
+  tempoutput.innerText = ''
+  output2 = result
+  output1 = ''
+})
+
+clear.addEventListener('click', (e) => {
+  underoutput.innerText = ''
+  overoutput.innerText = ''
+  tempoutput.innerText = ''
+  output1 = ''
+  output2 = ''
+  result = ''
+})
+
+del.addEventListener('click', (e) => {
+  underoutput.innerText = underoutput.innerText.toString().slice(0, -1)
+  output2 = output2.toString().slice(0, -1)
+})
+
+window.addEventListener('keydown', (e) => {
+  if (
+    e.key === '0' ||
+    e.key === '1' ||
+    e.key === '2' ||
+    e.key === '3' ||
+    e.key === '4' ||
+    e.key === '5' ||
+    e.key === '6' ||
+    e.key === '7' ||
+    e.key === '8' ||
+    e.key === '9' ||
+    e.key === '.' 
+  ) {
+    clicknumber(e.key)
+  } else if (
+    e.key === '+' ||
+    e.key === '-' ||
+    e.key === '/' ||
+    e.key === '%' 
+   ) {
+     clickoperation(e.key)
+   } else if (e.key === '*') {
+     clickoperation('x')
+   } else if (e.key === 'Enter' || e.key === '=') {
+     clickresult()
+   } else if (e.key === 'Backspace') {
+     clickdel()
+   } else if (e.key === 'Delete') {
+     clickclear()
+   }
+})
+
+function clicknumber(key) {
+  number.forEach((button) => {
+    if (button.innerText === key) {
+      button.click()
+    }
+  })
+}
+
+function clickoperation(key) {
+  operation.forEach((button) => {
+    if (button.innerText === key) {
+      button.click()
+    }
+  })
+}
+
+function clickresult() {
+  resultLast.click()
+}
+
+function clickdel() {
+  del.click()
+}
+
+function clickclear() {
+  clear.click()
 }
